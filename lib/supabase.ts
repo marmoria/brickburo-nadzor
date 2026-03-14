5a9b6a7870c7c5ea2913fbb607d97877039efc82
MaryMoro <marmoria@gmail.com>
2026-03-14 10:35:45 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,11 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://pjrvjlpjyfvlkobzkhtp.supabase.co'
-const SUPABASE_ANON_KEY = 'вставьте_сюда_anon_key_из_supabase'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqcnZqbHBqeWZ2bGtvYnpraHRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MjQ2NzksImV4cCI6MjA4OTAwMDY3OX0.hGPjFpMQ7n_xh-XE9qIINFpSPiOrK-zgYn_qY5TPDnY'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 export const supabaseAdmin = createClient(
   SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY || ''
-)
